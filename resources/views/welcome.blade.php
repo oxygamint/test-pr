c12796a009c15c63d1313c8b2cfdf84b36961e26
budistwn15 <budi.setiawan112001@gmail.com>
2024-03-01 14:10:27 +0700

    hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Yo man s</title>
+        <title>Yo man</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
